30a960cf57991e98f3b5a98ee43594eb7c56e1c1
TSprech <trey.sprecher@gmail.com>
2023-03-25 18:45:52 -0400

    Core Interference Example
    Example which lights up the onboard LED when the cores create a data conflict / race condition.

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,23 +1,41 @@
-/**
-* Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
-*
-* SPDX-License-Identifier: BSD-3-Clause
-*/
+#include "hardware/gpio.h"
 
-#include "pico/stdlib.h"
+#include "pico/multicore.h"
+#include "pico/stdio.h"
+#include "pico/sync.h"
+
+#include <cstdio>
+
+critical_section_t critical;
+volatile int a;
+volatile int b;
+
+void main1() {
+  do {
+    critical_section_enter_blocking(&critical);
+    a = a + 1;
+    b = b + 1;
+  } while (true);
+}
 
 int main() {
-#ifndef PICO_DEFAULT_LED_PIN
-#warning blink example requires a board with a regular LED
-#else
- const uint LED_PIN = PICO_DEFAULT_LED_PIN;
- gpio_init(LED_PIN);
- gpio_set_dir(LED_PIN, GPIO_OUT);
- while (true) {
-   gpio_put(LED_PIN, 1);
-   sleep_ms(250);
-   gpio_put(LED_PIN, 0);
-   sleep_ms(250);
- }
-#endif
+  gpio_init(PICO_DEFAULT_LED_PIN);
+  gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+
+  gpio_put(PICO_DEFAULT_LED_PIN, true);
+  sleep_ms(250);
+  gpio_put(PICO_DEFAULT_LED_PIN, false);
+  sleep_ms(250);
+
+  critical_section_init(&critical);
+
+  stdio_init_all();
+  multicore_launch_core1(main1);
+
+  do {
+    critical_section_enter_blocking(&critical);
+    if (a != b) {
+      gpio_put(PICO_DEFAULT_LED_PIN, true);
+    }
+  } while (true);
 }
